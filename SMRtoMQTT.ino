d2ba6af3958e7df532f26004998df39d2fd5a1f9
Reinier van der Leer <reintjebadboy@gmail.com>
2019-09-02 16:13:22 +0200

    per-metric if-else block -> type-based handling

--- SMRtoMQTT.ino
+++ SMRtoMQTT.ino
@@ -11,36 +11,60 @@ const char* mqttServ  = "MQTT_SERV_IP";
 const int   mqttPort  = 1883;
 const char* clientID  = "EnergyMonitor";
 
-WiFiClient espClient;
-PubSubClient client(espClient);
-char msg[50];
 
-// MQTT topic settings: empty topic will disable publishing 
+typedef struct {
+  char ident[12];
+  int type;
+  char topic[72];
+  char description[48];
+} metricDef;
 
-#define ENERGY_DEL_LOW_TOPIC        "whiskeygrid/energy/mains/reading/delivered/low";
-#define ENERGY_DEL_HIGH_TOPIC       "whiskeygrid/energy/mains/reading/delivered/high";
-#define ENERGY_REDEL_LOW_TOPIC      "whiskeygrid/energy/mains/reading/redelivered/low";
-#define ENERGY_REDEL_HIGH_TOPIC     "whiskeygrid/energy/mains/reading/redelivered/high";
-#define TARIFF_TOPIC                "whiskeygrid/energy/mains/reading/tariff";
-#define P_TOTAL_DEL_TOPIC           "whiskeygrid/energy/mains/power/total";
-#define P_TOTAL_REDEL_TOPIC         "whiskeygrid/energy/mains/power/total_redelivery";
-#define P_L1_TOPIC                  "whiskeygrid/energy/mains/power/L1"
-#define P_L1_REDEL_TOPIC            "whiskeygrid/energy/mains/power/L1_redelivery"
-#define V_L1_TOPIC                  "whiskeygrid/energy/mains/voltage/L1"
-#define I_L1_TOPIC                  "whiskeygrid/energy/mains/current/L1"
+#define METRIC_TYPE_META      1
+#define METRIC_TYPE_META_TEXT 2
+#define METRIC_TYPE_FLOAT    11
+#define METRIC_TYPE_TEXT     12
+#define METRIC_TYPE_BARE     13
+#define METRIC_TYPE_GAS      21
+#define METRIC_TYPE_OTHER    99
 
-#define GAS_READING_TOPIC           "whiskeygrid/energy/gas/reading";
+// Metric and MQTT topic settings: comment a line to disable processing/publishing of the metric
 
-#define VOLTAGE_SAGS_L1_TOPIC       "whiskeygrid/energy/mains/report/voltage_sags/L1"
-#define VOLTAGE_SWELLS_L1_TOPIC     "whiskeygrid/energy/mains/report/voltage_swells/L1"
-#define POWERFAILURES_TOPIC         "whiskeygrid/energy/mains/report/powerfailures";
-#define POWERFAILURES_LONG_TOPIC    "whiskeygrid/energy/mains/report/powerfailures_long";
-#define POWERFAILURE_DETAILS_TOPIC  "whiskeygrid/energy/mains/report/powerfailure_details";
-#define TEXT_MESSAGE_TOPIC          "whiskeygrid/energy/mains/message";
+metricDef metricDefs[] = {
+  { "1-3:0.2.8",   METRIC_TYPE_META,  "",       "SMR protocol version" },
+  { "0-0:1.0.0",   METRIC_TYPE_META,  "",       "telegram timestamp" },
+  { "0-0:96.1.1",  METRIC_TYPE_META_TEXT,  "",  "meter serial number" },
+
+  { "1-0:1.8.1",   METRIC_TYPE_FLOAT, "whiskeygrid/energy/mains/reading/delivered/low",     "total delivered energy (low tariff)" },
+  { "1-0:1.8.2",   METRIC_TYPE_FLOAT, "whiskeygrid/energy/mains/reading/delivered/high",    "total delivered energy (high tariff)" },
+  { "1-0:2.8.1",   METRIC_TYPE_FLOAT, "whiskeygrid/energy/mains/reading/redelivered/low",   "total redelivered (low tariff)" },
+  { "1-0:2.8.2",   METRIC_TYPE_FLOAT, "whiskeygrid/energy/mains/reading/redelivered/high",  "total redelivered (high tariff)" },
+  { "0-0:96.14.0", METRIC_TYPE_BARE,  "whiskeygrid/energy/mains/reading/tariff",            "tariff" },
+  { "1-0:1.7.0",   METRIC_TYPE_FLOAT, "whiskeygrid/energy/mains/power/total",               "total power" },
+  { "1-0:2.7.0",   METRIC_TYPE_FLOAT, "whiskeygrid/energy/mains/power/total_redelivery",    "total redelivery power" },
+  { "1-0:21.7.0",  METRIC_TYPE_FLOAT, "whiskeygrid/energy/mains/power/L1",                  "L1 power" },
+  { "1-0:22.7.0",  METRIC_TYPE_FLOAT, "whiskeygrid/energy/mains/power/L1_redelivery",       "L1 redelivery power" },
+  { "1-0:32.7.0",  METRIC_TYPE_FLOAT, "whiskeygrid/energy/mains/voltage/L1",                "L1 voltage" },
+  { "1-0:31.7.0",  METRIC_TYPE_FLOAT, "whiskeygrid/energy/mains/current/L1",                "L1 current" },
+
+  { "0-1:24.1.0",  METRIC_TYPE_BARE,      "",   "gas meter device type" },
+  { "0-1:96.1.0",  METRIC_TYPE_META_TEXT, "",   "gas meter serial number" },
+
+  { "0-1:24.2.1",  METRIC_TYPE_GAS,   "whiskeygrid/energy/gas/reading",   "gas meter last reading" },
+
+  { "0-0:96.7.21", METRIC_TYPE_BARE,  "whiskeygrid/energy/mains/report/powerfailures",        "power failures" },
+  { "0-0:96.7.9",  METRIC_TYPE_BARE,  "whiskeygrid/energy/mains/report/powerfailures_long",   "long power failures" },
+  { "1-0:99.97.0", METRIC_TYPE_OTHER, "whiskeygrid/energy/mains/report/powerfailure_details", "power failure event log" },
+  { "1-0:32.32.0", METRIC_TYPE_BARE,  "whiskeygrid/energy/mains/report/voltage_sags/L1",      "L1 voltage sags" },
+  { "1-0:32.36.0", METRIC_TYPE_BARE,  "whiskeygrid/energy/mains/report/voltage_swells/L1",    "L1 voltage swells" },
+
+  { "0-0:96.13.0", METRIC_TYPE_TEXT,  "whiskeygrid/energy/mains/message",   "text message" }  // text message
+};
 
 const int interval = 15000;
 int lastTelegram = -15000;
+
 const int RTSpin = 5;
+// #define RTS_INVERT_LOGIC  // to invert the RTS output logic, e.g. to use with a NPN output inverter when the GPIO output is too low, uncomment this line:
 
 byte bufferIn[768];
 int readLength;
@@ -49,6 +73,10 @@ char receivedCRC[5];
 // byte bufferIn[768] = "/ISK5\\2M550E-1012\r\n\r\n1-3:0.2.8(50)\r\n0-0:1.0.0(190827155511S)\r\n0-0:96.1.1(4D455445525F53455249414C235F484558)\r\n1-0:1.8.1(000057.460*kWh)\r\n1-0:1.8.2(000037.300*kWh)\r\n1-0:2.8.1(000000.000*kWh)\r\n1-0:2.8.2(000000.000*kWh)\r\n0-0:96.14.0(0002)\r\n1-0:1.7.0(00.498*kW)\r\n1-0:2.7.0(00.000*kW)\r\n0-0:96.7.21(00008)\r\n0-0:96.7.9(00002)\r\n1-0:99.97.0()\r\n1-0:32.32.0(00005)\r\n1-0:32.36.0(00001)\r\n0-0:96.13.0()\r\n1-0:32.7.0(235.4*V)\r\n1-0:31.7.0(002*A)\r\n1-0:21.7.0(00.454*kW)\r\n1-0:22.7.0(00.000*kW)\r\n0-1:24.1.0(003)\r\n0-1:96.1.0(4D455445525F53455249414C235F484558)\r\n0-1:24.2.1(190827155507S)(00004.380*m3)\r\n!";
 // int readLength = strlen((char*)bufferIn);
 // char receivedCRC[5] = "ECDF";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+char msg[50];
 
 
 void setup()
@@ -121,12 +149,20 @@ void loop()
 }
 
 
+#ifdef RTS_INVERT_LOGIC
+  #define RTS_HIGH LOW
+  #define RTS_LOW HIGH
+#else
+  #define RTS_HIGH HIGH
+  #define RTS_LOW LOW
+#endif
+
 void requestTelegram()
 {
   Serial.println("Requesting telegram...");
-  digitalWrite(RTSpin, HIGH);
+  digitalWrite(RTSpin, RTS_HIGH);
   delay(100);
-  digitalWrite(RTSpin, LOW);
+  digitalWrite(RTSpin, RTS_LOW);
 }
 
 
@@ -147,23 +183,19 @@ String lastGasTimestamp = "", lastTextMessage = "";
 
 void parseTelegram(char* telegram)
 {
-  int i = 0, valueStart;
-  bool valueIsFloat = false;
+  int i = 0;
   char *lineptr;
+  String line, ident, value, tmpValue, timestamp, gasTimestamp;
   byte hexbuf[3];
-  String line, ident, value, tmpValue, description = "", topic = "", timestamp, gasTimestamp;
-  line.reserve(64);
-  ident.reserve(12);
-  value.reserve(50);
+  metricDef* metric;
+  bool allowPublish;
 
   Serial.print("==== START OF TELEGRAM ====\n\n");
 
   lineptr = strtok(telegram, "\r\n");
-  line = String(lineptr);
+  line = String(lineptr);   // need to do anything with the device model header?
 
   Serial.print(line + "\n\n");
-
-  // need to do anything with the device model header?
 
   while (lineptr = strtok(NULL, "\r\n")) {
     line = String(lineptr);
@@ -173,188 +205,98 @@ void parseTelegram(char* telegram)
       break;
     }
 
-    i++;
-    Serial.print(i);
+    Serial.print(++i);
     Serial.print(": ");
     Serial.print(line);
 
     if (line.length() >= 8) {
-      valueStart = line.lastIndexOf('(') + 1;
+      allowPublish = true;
 
-      if (valueStart != -1) {
-        value = line.substring(valueStart, line.lastIndexOf(')'));
-
+      if (line.lastIndexOf('(') != -1) {
+        value = line.substring(line.lastIndexOf('(') + 1, line.lastIndexOf(')'));
 
         ident = line.substring(0, line.indexOf('('));
 
-        if (ident.equals("1-3:0.2.8")) {          // protocol version (should be 50)
-          //TODO: use this value to adjust protocol handling
-          topic = "";
-          description = "SMR version";
-          valueIsFloat = false;
+        metric = getMetricDef(ident.c_str());
 
-        } else if (ident.equals("0-0:1.0.0")) {   // timestamp
-          topic = description = "";
-          valueIsFloat = false;
+        if (metric != NULL) {
+          switch (metric->type) {
+            case METRIC_TYPE_FLOAT:
+              value.replace("*", " ");
+              break;
 
-          timestamp = value.substring(0, value.length() - 1);
+            // case METRIC_TYPE_BARE:
+            //   // don't do anything
+            //   break;
 
-        } else if (ident.equals("0-0:96.1.1")) {  // serial number (ascii hex) - is this useful?
-          topic = "";
-          description = "serial number";
-          valueIsFloat = false;
+            case METRIC_TYPE_GAS:
+              gasTimestamp = line.substring(line.indexOf('(') + 1, line.indexOf(')') - 1);
+              Serial.print("\t@{" + gasTimestamp + "}");
 
-          tmpValue = "";
-          tmpValue.reserve(value.length()/2);
+              allowPublish = gasTimestamp > lastGasTimestamp;
 
-          for (int i = 0; i < value.length()/2; i++) {
-            value.substring(i*2).getBytes(hexbuf, 3);
-            hexbuf[2] = 0;
+              lastGasTimestamp = gasTimestamp;
+              break;
 
-            tmpValue.concat((char)strtol((char*)hexbuf, NULL, 16));
+            case METRIC_TYPE_TEXT:
+            case METRIC_TYPE_META_TEXT:
+              tmpValue = "";
+              tmpValue.reserve(value.length()/2);
+
+              for (int i = 0; i < value.length()/2; i++) {
+                value.substring(i*2).getBytes(hexbuf, 3);
+                hexbuf[2] = 0;
+
+                tmpValue.concat((char)strtol((char*)hexbuf, NULL, 16));
+              }
+              value = tmpValue;
+
+              if (metric->type == METRIC_TYPE_TEXT) break;
+
+            case METRIC_TYPE_META:
+
+              if (strcmp("1-3:0.2.8", metric->ident) == 0) { // SMR protocol version
+                //TODO: use this value to adjust protocol handling
+
+              } else if (strcmp("0-0:1.0.0", metric->ident) == 0) { // timestamp
+                timestamp = value.substring(0, value.length() - 1); // cut off DST indicator
+
+              } else if (strcmp("0-0:96.1.1", metric->ident) == 0) {  // device serial number
+                // is this useful?
+
+              } else if (strcmp("0-1:96.1.0", metric->ident) == 0) {  // gas meter serial number
+                // is this useful?
+              }
+              break;
           }
-          value = tmpValue;
-
-        } else if (ident.equals("1-0:1.8.1")) {   // total delivered energy (low tariff)
-          topic = ENERGY_DEL_LOW_TOPIC;
-          description = "total delivered energy (low tariff)";
-          valueIsFloat = true;
-
-        } else if (ident.equals("1-0:1.8.2")) {   // total delivered energy (high tariff)
-          topic = ENERGY_DEL_HIGH_TOPIC;
-          description = "total delivered energy (high tariff)";
-          valueIsFloat = true;
-
-        } else if (ident.equals("1-0:2.8.1")) {   // total redelivery (low tariff)
-          topic = ENERGY_REDEL_LOW_TOPIC;
-          description = "total redelivered (low tariff)";
-          valueIsFloat = true;
-
-        } else if (ident.equals("1-0:2.8.2")) {   // total redelivery (high tariff)
-          topic = ENERGY_REDEL_HIGH_TOPIC;
-          description = "total redelivered (high tariff)";
-          valueIsFloat = true;
-
-        } else if (ident.equals("0-0:96.14.0")) { // tariff indicator
-          topic = TARIFF_TOPIC;
-          description = "tariff";
-          valueIsFloat = false;
-
-        } else if (ident.equals("1-0:1.7.0")) {   // actual total power
-          topic = P_TOTAL_DEL_TOPIC;
-          description = "total power";
-          valueIsFloat = true;
-
-        } else if (ident.equals("1-0:2.7.0")) {   // actual total redelivery power
-          topic = P_TOTAL_REDEL_TOPIC;
-          description = "total redelivery power";
-          valueIsFloat = true;
-
-        } else if (ident.equals("0-0:96.7.21")) { // # of power failures
-          topic = POWERFAILURES_TOPIC;
-          description = "power failures";
-          valueIsFloat = false;
-
-        } else if (ident.equals("0-0:96.7.9")) {  // # of long power failures
-          topic = POWERFAILURES_LONG_TOPIC;
-          description = "long power failures";
-          valueIsFloat = false;
-
-        } else if (ident.equals("1-0:99.97.0")) { // power failure event log (time + duration)
-          topic = POWERFAILURE_DETAILS_TOPIC;
-          description = "power failure event log";
-
-        } else if (ident.equals("1-0:32.32.0")) { // # of voltage sags in L1
-          topic = VOLTAGE_SAGS_L1_TOPIC;
-          description = "L1 voltage sags";
-          valueIsFloat = false;
-
-        } else if (ident.equals("1-0:32.36.0")) { // # of voltage swells in L1
-          topic = VOLTAGE_SWELLS_L1_TOPIC;
-          description = "L1 voltage swells";
-          valueIsFloat = false;
-
-        } else if (ident.equals("0-0:96.13.0")) { // text message
-          topic = TEXT_MESSAGE_TOPIC;
-          description = "text message";
-          valueIsFloat = false;
-
-        } else if (ident.equals("1-0:32.7.0")) {  // actual L1 voltage
-          topic = V_L1_TOPIC;
-          description = "L1 voltage";
-          valueIsFloat = true;
-
-        } else if (ident.equals("1-0:31.7.0")) {  // actual L1 current
-          topic = I_L1_TOPIC;
-          description = "L1 current";
-          valueIsFloat = true;
-
-        } else if (ident.equals("1-0:21.7.0")) {  // actual L1 power
-          topic = P_L1_TOPIC;
-          description = "L1 power";
-          valueIsFloat = true;
-
-        } else if (ident.equals("1-0:22.7.0")) {  // actual L1 redelivery power
-          topic = P_L1_REDEL_TOPIC;
-          description = "L1 redelivery power";
-          valueIsFloat = true;
-
-        } else if (ident.equals("0-1:24.1.0")) {  // gas meter device type
-          topic = "";
-          description = "gas meter device type";
-          valueIsFloat = false;
-
-        } else if (ident.equals("0-1:96.1.0")) {  // gas meter serial number (ascii hex)
-          topic = "";
-          description = "gas meter SN";
-          valueIsFloat = false;
-
-          tmpValue = "";
-          tmpValue.reserve(value.length()/2);
-
-          for (int i = 0; i < value.length()/2; i++) {
-            value.substring(i*2).getBytes(hexbuf, 3);
-            hexbuf[2] = 0;
-
-            tmpValue.concat((char)strtol((char*)hexbuf, NULL, 16));
-          }
-          value = tmpValue;
-
-        } else if (ident.equals("0-1:24.2.1")) {  // gas meter last reading (timestamp + reading)
-          description = "gas meter last reading";
-          valueIsFloat = true;
-
-          gasTimestamp = line.substring(line.indexOf('(') + 1, line.indexOf(')') - 1);
-          Serial.print("\t@{" + gasTimestamp + "}");
-
-          if (gasTimestamp > lastGasTimestamp) {
-            topic = GAS_READING_TOPIC;
-          } else {
-            topic = "";
-          }
-
-          lastGasTimestamp = gasTimestamp;
-
-        } else {  // ident not known
-          topic = description = "";
-          valueIsFloat = false;
         }
 
-        if (valueIsFloat)  value.replace("*", " ");
+        if (strlen(metric->description) > 0)
+          Serial.print("  -> " + metric->description + " [" + value + "]");
 
-        if (description.length() > 0) {
-          Serial.print("  -> " + description + " [" + value + "]");
-        }
-
-        if (topic.length() > 0) {
-          client.publish(topic.c_str(), value.c_str(), true);
-        }
+        if (allowPublish && strlen(metric->topic) > 0)
+          client.publish(metric->topic, value.c_str(), true);
       }
     }
     Serial.print("\n\n");
   }
 
 }
+
+
+metricDef* getMetricDef(const char* ident)
+{
+  for (int i = 0; i < sizeof(metricDefs)/sizeof(metricDefs[0]); i++)
+  {
+    if (strcmp(ident, metricDefs[i].ident) == 0) {
+      return &metricDefs[i];
+    }
+  }
+  return NULL;
+}
+
+
+// WiFi set-up and reconnect functions
 
 void setupWifi()
 {
