3ed49031fee0dba2702c309103c73d58f50c1f29
Reinier van der Leer <reintjebadboy@gmail.com>
2019-10-22 14:26:16 +0200

    Moved settings to settings.h, cleaned up

--- SMRtoMQTT.ino
+++ SMRtoMQTT.ino
@@ -1,98 +1,18 @@
 #include <ESP8266WiFi.h>
+
 #include <PubSubClient.h>
-// to use PubSubClient with influx publishing enabled, the MAX_PACKET_SIZE constant in PubSubClient.h must be set to at least 1024 (to be sure)
+// To use PubSubClient with influx publishing enabled, the MAX_PACKET_SIZE constant in PubSubClient.h must be set to at least 1024 (to be sure)
 
 #include "Crc16.h"
 Crc16 CRC;
 
-// WiFi and MQTT stuff
-const char* ssid      = "NETWORK";
-const char* password  = "WIFI_PASSWORD";
-const char* mqttServ  = "MQTT_SERV_IP";
-const int   mqttPort  = 1883;
-const char* clientID  = "EnergyMonitor";
-const char* connTopic = "whiskeygrid/debug/node_connect";
-
-#define INFLUX  // comment to disable influx
-#ifdef INFLUX
-  const char* influxTopic = "whiskeygrid/energy/influx";
-  const char* influxElectricityMeasurement = "electricity";
-  const char* influxGasMeasurement = "gas";
-#endif
-
-typedef struct {
-  char ident[12];
-  int type;
-  char influx_column[24];
-  char topic[72];
-  char description[48];
-} metricDef;
-
-#define METRIC_TYPE_META      1
-#define METRIC_TYPE_META_TEXT 2
-#define METRIC_TYPE_FLOAT    11
-#define METRIC_TYPE_TEXT     12
-#define METRIC_TYPE_BARE     13
-#define METRIC_TYPE_GAS      21
-#define METRIC_TYPE_OTHER    99
-
-// Metric and MQTT topic settings: comment a line to disable processing/publishing of the metric
-// leaving the third entry (influx_column) empty will disable inclusion in the influx formatted metric, if enabled
-
-metricDef metricDefs[] = {
-//{ "OBIS ref.",    METRIC_TYPE_CONSTANT,   "influx col.",    "MQTT topic", "description (only for serial debug output)" },
-  { "1-3:0.2.8",    METRIC_TYPE_META,       "",               "",           "SMR protocol version"  },
-  { "0-0:1.0.0",    METRIC_TYPE_META,       "timestamp",      "",           "telegram timestamp"    },
-  { "0-0:96.1.1",   METRIC_TYPE_META_TEXT,  "meter_sn",       "",           "meter serial number"   },
-
-  { "1-0:1.8.1",    METRIC_TYPE_FLOAT,  "delivered_low",      "whiskeygrid/energy/mains/reading/delivered/low",     "total delivered energy (low tariff)" },
-  { "1-0:1.8.2",    METRIC_TYPE_FLOAT,  "delivered_high",     "whiskeygrid/energy/mains/reading/delivered/high",    "total delivered energy (high tariff)" },
-  { "1-0:2.8.1",    METRIC_TYPE_FLOAT,  "redelivered_low",    "whiskeygrid/energy/mains/reading/redelivered/low",   "total redelivered (low tariff)" },
-  { "1-0:2.8.2",    METRIC_TYPE_FLOAT,  "redelivered_high",   "whiskeygrid/energy/mains/reading/redelivered/high",  "total redelivered (high tariff)" },
-  { "0-0:96.14.0",  METRIC_TYPE_BARE,   "tariff",             "whiskeygrid/energy/mains/reading/tariff",            "tariff" },
-  { "1-0:1.7.0",    METRIC_TYPE_FLOAT,  "P_total",            "whiskeygrid/energy/mains/power/total",               "total power" },
-  { "1-0:2.7.0",    METRIC_TYPE_FLOAT,  "P_total_redelivery", "whiskeygrid/energy/mains/power/total_redelivery",    "total redelivery power" },
-  { "1-0:21.7.0",   METRIC_TYPE_FLOAT,  "P_L1",               "whiskeygrid/energy/mains/power/L1",                  "L1 power" },
-  { "1-0:22.7.0",   METRIC_TYPE_FLOAT,  "P_L1_redelivery",    "whiskeygrid/energy/mains/power/L1_redelivery",       "L1 redelivery power" },
-  { "1-0:32.7.0",   METRIC_TYPE_FLOAT,  "V_L1",               "whiskeygrid/energy/mains/voltage/L1",                "L1 voltage" },
-  { "1-0:31.7.0",   METRIC_TYPE_FLOAT,  "I_L1",               "whiskeygrid/energy/mains/current/L1",                "L1 current" },
-  // { "1-0:41.7.0",   METRIC_TYPE_FLOAT,  "P_L2",               "whiskeygrid/energy/mains/power/L2",                  "L2 power" },
-  // { "1-0:42.7.0",   METRIC_TYPE_FLOAT,  "P_L2_redelivery",    "whiskeygrid/energy/mains/power/L2_redelivery",       "L2 redelivery power" },
-  // { "1-0:52.7.0",   METRIC_TYPE_FLOAT,  "V_L2",               "whiskeygrid/energy/mains/voltage/L2",                "L2 voltage" },
-  // { "1-0:51.7.0",   METRIC_TYPE_FLOAT,  "I_L2",               "whiskeygrid/energy/mains/current/L2",                "L2 current" },
-  // { "1-0:61.7.0",   METRIC_TYPE_FLOAT,  "P_L3",               "whiskeygrid/energy/mains/power/L3",                  "L3 power" },
-  // { "1-0:62.7.0",   METRIC_TYPE_FLOAT,  "P_L3_redelivery",    "whiskeygrid/energy/mains/power/L3_redelivery",       "L3 redelivery power" },
-  // { "1-0:72.7.0",   METRIC_TYPE_FLOAT,  "V_L3",               "whiskeygrid/energy/mains/voltage/L3",                "L3 voltage" },
-  // { "1-0:71.7.0",   METRIC_TYPE_FLOAT,  "I_L3",               "whiskeygrid/energy/mains/current/L3",                "L3 current" },
-
-  { "0-1:24.1.0",   METRIC_TYPE_BARE,       "",               "",   "gas meter device type" },
-  { "0-1:96.1.0",   METRIC_TYPE_META_TEXT,  "gas_meter_sn",   "",   "gas meter serial number" },
-
-  { "0-1:24.2.1",   METRIC_TYPE_GAS,    "gas_reading",    "whiskeygrid/energy/gas/reading",   "gas meter last reading" },
-
-  { "0-0:96.7.21",  METRIC_TYPE_BARE,   "failures",       "whiskeygrid/energy/mains/report/powerfailures",        "power failures" },
-  { "0-0:96.7.9",   METRIC_TYPE_BARE,   "long_failures",  "whiskeygrid/energy/mains/report/powerfailures_long",   "long power failures" },
-  { "1-0:99.97.0",  METRIC_TYPE_OTHER,  "failure_log",    "whiskeygrid/energy/mains/report/powerfailure_details", "power failure event log" },
-  { "1-0:32.32.0",  METRIC_TYPE_BARE,   "L1_sags",        "whiskeygrid/energy/mains/report/voltage_sags/L1",      "L1 voltage sags" },
-  // { "1-0:52.32.0",  METRIC_TYPE_BARE,   "L2_sags",        "whiskeygrid/energy/mains/report/voltage_sags/L2",      "L2 voltage sags" },
-  // { "1-0:72.32.0",  METRIC_TYPE_BARE,   "L3_sags",        "whiskeygrid/energy/mains/report/voltage_sags/L3",      "L3 voltage sags" },
-  { "1-0:32.36.0",  METRIC_TYPE_BARE,   "L1_swells",      "whiskeygrid/energy/mains/report/voltage_swells/L1",    "L1 voltage swells" },
-  // { "1-0:52.36.0",  METRIC_TYPE_BARE,   "L2_swells",      "whiskeygrid/energy/mains/report/voltage_swells/L2",    "L2 voltage swells" },
-  // { "1-0:72.36.0",  METRIC_TYPE_BARE,   "L3_swells",      "whiskeygrid/energy/mains/report/voltage_swells/L3",    "L3 voltage swells" },
-
-  { "0-0:96.13.0",  METRIC_TYPE_TEXT,   "message",   "whiskeygrid/energy/mains/message",   "text message" }  // text message
-};
-
-const int interval = 15000;
-int lastTelegram = -15000;
-
-const int RTSpin = 5;
-// #define RTS_INVERT_LOGIC  // to invert the RTS output logic, e.g. to use with a NPN output inverter when the GPIO output is too low, uncomment this line:
+#include "settings.h"
 
 byte bufferIn[768];
 int readLength;
 char receivedCRC[5];
 
+/* Valid telegram for testing purposes */
 // byte bufferIn[768] = "/ISK5\\2M550E-1012\r\n\r\n1-3:0.2.8(50)\r\n0-0:1.0.0(190827155511S)\r\n0-0:96.1.1(4D455445525F53455249414C235F484558)\r\n1-0:1.8.1(000057.460*kWh)\r\n1-0:1.8.2(000037.300*kWh)\r\n1-0:2.8.1(000000.000*kWh)\r\n1-0:2.8.2(000000.000*kWh)\r\n0-0:96.14.0(0002)\r\n1-0:1.7.0(00.498*kW)\r\n1-0:2.7.0(00.000*kW)\r\n0-0:96.7.21(00008)\r\n0-0:96.7.9(00002)\r\n1-0:99.97.0()\r\n1-0:32.32.0(00005)\r\n1-0:32.36.0(00001)\r\n0-0:96.13.0()\r\n1-0:32.7.0(235.4*V)\r\n1-0:31.7.0(002*A)\r\n1-0:21.7.0(00.454*kW)\r\n1-0:22.7.0(00.000*kW)\r\n0-1:24.1.0(003)\r\n0-1:96.1.0(4D455445525F53455249414C235F484558)\r\n0-1:24.2.1(190827155507S)(00004.380*m3)\r\n!";
 // int readLength = strlen((char*)bufferIn);
 // char receivedCRC[5] = "ECDF";
@@ -110,10 +30,13 @@ char msg[50];
   #define RTS_LOW LOW
 #endif
 
+int lastTelegram = -15000;
+
 void setup()
 {
   Serial.begin(115200);
   Serial.setTimeout(100);
+
   pinMode(RTSpin, OUTPUT);
   digitalWrite(RTSpin, RTS_LOW);
 
@@ -132,7 +55,6 @@ void loop()
 
   if (millis() - lastTelegram > interval) {
     if (Serial.available() > 0) {     // check for incoming serial data
-      // while (Serial.available() > 0)  Serial.read();
       if (Serial.peek() == '/') {     // check for telegram header
         readLength = Serial.readBytesUntil('!', bufferIn, 766);
         bufferIn[readLength++] = '!';
@@ -142,7 +64,6 @@ void loop()
 
         Serial.print("telegram length: ");
         Serial.println(readLength);
-        // Serial.println(bufferIn);
 
         Serial.readBytes(receivedCRC, 4);
         receivedCRC[4] = 0;
@@ -238,7 +159,7 @@ void parseTelegram(char* telegram)
   Serial.print("==== START OF TELEGRAM ====\n\n");
 
   lineptr = strtok(telegram, "\r\n");
-  line = String(lineptr);   // need to do anything with the device model header?
+  line = String(lineptr);   // TODO: need to do anything with the device model header?
 
   Serial.print(line + "\n\n");
 
@@ -298,8 +219,6 @@ void parseTelegram(char* telegram)
               break;
 
             case METRIC_TYPE_GAS:
-              // gasTimestamp = line.substring(line.indexOf('(') + 1, line.indexOf(')') - 1);
-              // gasTimestampDST = line.charAt(line.indexOf(')') - 1) == 'S';
               gasTimestamp = line.substring(line.indexOf('(') + 1, line.indexOf(')'));
               Serial.print("\t@{" + gasTimestamp + "}");
 
@@ -358,9 +277,6 @@ void parseTelegram(char* telegram)
             case METRIC_TYPE_META:
 
               if (strcmp("0-0:1.0.0", metric->ident) == 0) { // timestamp
-                // timestamp = value.substring(0, value.length() - 1); // cut off DST indicator
-                // timestampDST = value.charAt(value.length() - 1) == 'S';
-
                 #ifdef INFLUX
                   if (strlen(metric->influx_column) > 0)
                     appendInfluxValue(&influxFields, metric->influx_column, value, true);
@@ -374,9 +290,6 @@ void parseTelegram(char* telegram)
 
               // } else if (strcmp("1-3:0.2.8", metric->ident) == 0) { // SMR protocol version
               //   //TODO: use this value to adjust protocol handling
-
-              // } else if (strcmp("0-0:96.1.1", metric->ident) == 0) {  // device serial number
-              //   // is this useful?
 
               } else {
                 #ifdef INFLUX
@@ -432,18 +345,18 @@ void appendInfluxValue(String* influxString, char* column_name, String value, bo
 }
 
 
-// WiFi set-up and reconnect functions
+// WiFi set-up and MQTT reconnect functions
 
 void setupWifi()
 {
   delay(10);
-  // We start by connecting to a WiFi network
+  // Connect to WiFi network
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
 
-  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
+  WiFi.mode(WIFI_STA);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -459,14 +372,14 @@ void setupWifi()
 void reconnect()
 {
   int connLoseMillis = millis();
-  // Loop until we're reconnected
+  // Loop until connected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
 
     if (client.connect(clientID)) { // Attempt to connect
       Serial.println("connected");
 
-      // Once connected, publish an announcement...
+      // Post connect message to MQTT topic once connected
       int connectMillis = millis();
       float lostSeconds = (connectMillis - connLoseMillis) / 1000;
       char lostSecStr[6];
@@ -475,7 +388,6 @@ void reconnect()
       client.publish(connTopic, msg);
       Serial.println(msg);
 
-      // client.subscribe(callTopic);  // ... and resubscribe
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
---
+++ settings.example.h
@@ -0,0 +1,96 @@
+/* WiFi and MQTT stuff */
+const char* ssid      = "WIFI_NETWORK";
+const char* password  = "WIFI_PASSWORD";
+
+const char* mqttServ  = "MQTT_SERV_IP";
+const int   mqttPort  = 1883;
+const char* clientID  = "Metertrekker";
+const char* connTopic = "my_mqtt_root/debug/node_connect";
+
+/* Comment next line to disable influx: */
+#define INFLUX
+
+#ifdef INFLUX
+  const char* influxTopic = "my_mqtt_root/energy/influx";
+  const char* influxElectricityMeasurement = "electricity";
+  const char* influxGasMeasurement = "gas";
+#endif
+
+/* Telegram request interval */
+const int interval = 15000;
+
+/* To invert the RTS output logic, e.g. to use with an output inverter when the GPIO output is too low, uncomment this line: */
+#define RTS_INVERT_LOGIC
+const int RTSpin = D1;
+
+/* MQTT topic & InfluxDB column configuration */
+
+typedef struct {
+  char ident[12];
+  int type;
+  char influx_column[24];
+  char topic[72];
+  char description[48];
+} metricDef;
+
+#define METRIC_TYPE_META      1
+#define METRIC_TYPE_META_TEXT 2
+#define METRIC_TYPE_FLOAT    11
+#define METRIC_TYPE_TEXT     12
+#define METRIC_TYPE_BARE     13
+#define METRIC_TYPE_GAS      21
+#define METRIC_TYPE_OTHER    99
+
+/* Uncomment to enable metrics for three phase power: */
+// #define THREE_PHASE
+
+/* Metric and MQTT topic settings: comment a line to disable processing/publishing of the metric.
+ * Leaving the third entry (influx_column) empty will disable inclusion in the influx line, if enabled */
+
+metricDef metricDefs[] = {
+//{ "OBIS ref.",    METRIC_TYPE_CONSTANT,   "influx col.",    "MQTT topic", "description (only for serial debug output)" },
+  { "1-3:0.2.8",    METRIC_TYPE_META,       "",               "",           "SMR protocol version"  },
+  { "0-0:1.0.0",    METRIC_TYPE_META,       "timestamp",      "",           "telegram timestamp"    },
+  { "0-0:96.1.1",   METRIC_TYPE_META_TEXT,  "meter_sn",       "",           "meter serial number"   },
+
+  { "1-0:1.8.1",    METRIC_TYPE_FLOAT,  "delivered_low",      "my_mqtt_root/energy/mains/reading/delivered/low",    "total delivered energy (low tariff)" },
+  { "1-0:1.8.2",    METRIC_TYPE_FLOAT,  "delivered_high",     "my_mqtt_root/energy/mains/reading/delivered/high",   "total delivered energy (high tariff)" },
+  { "1-0:2.8.1",    METRIC_TYPE_FLOAT,  "redelivered_low",    "my_mqtt_root/energy/mains/reading/redelivered/low",  "total redelivered (low tariff)" },
+  { "1-0:2.8.2",    METRIC_TYPE_FLOAT,  "redelivered_high",   "my_mqtt_root/energy/mains/reading/redelivered/high", "total redelivered (high tariff)" },
+  { "0-0:96.14.0",  METRIC_TYPE_BARE,   "tariff",             "my_mqtt_root/energy/mains/reading/tariff",           "tariff" },
+  { "1-0:1.7.0",    METRIC_TYPE_FLOAT,  "P_total",            "my_mqtt_root/energy/mains/power/total",              "total power" },
+  { "1-0:2.7.0",    METRIC_TYPE_FLOAT,  "P_total_redelivery", "my_mqtt_root/energy/mains/power/total_redelivery",   "total redelivery power" },
+  { "1-0:21.7.0",   METRIC_TYPE_FLOAT,  "P_L1",               "my_mqtt_root/energy/mains/power/L1",                 "L1 power" },
+  { "1-0:22.7.0",   METRIC_TYPE_FLOAT,  "P_L1_redelivery",    "my_mqtt_root/energy/mains/power/L1_redelivery",      "L1 redelivery power" },
+  { "1-0:32.7.0",   METRIC_TYPE_FLOAT,  "V_L1",               "my_mqtt_root/energy/mains/voltage/L1",               "L1 voltage" },
+  { "1-0:31.7.0",   METRIC_TYPE_FLOAT,  "I_L1",               "my_mqtt_root/energy/mains/current/L1",               "L1 current" },
+  #ifdef THREE_PHASE
+  { "1-0:41.7.0",   METRIC_TYPE_FLOAT,  "P_L2",               "my_mqtt_root/energy/mains/power/L2",                 "L2 power" },
+  { "1-0:42.7.0",   METRIC_TYPE_FLOAT,  "P_L2_redelivery",    "my_mqtt_root/energy/mains/power/L2_redelivery",      "L2 redelivery power" },
+  { "1-0:52.7.0",   METRIC_TYPE_FLOAT,  "V_L2",               "my_mqtt_root/energy/mains/voltage/L2",               "L2 voltage" },
+  { "1-0:51.7.0",   METRIC_TYPE_FLOAT,  "I_L2",               "my_mqtt_root/energy/mains/current/L2",               "L2 current" },
+  { "1-0:61.7.0",   METRIC_TYPE_FLOAT,  "P_L3",               "my_mqtt_root/energy/mains/power/L3",                 "L3 power" },
+  { "1-0:62.7.0",   METRIC_TYPE_FLOAT,  "P_L3_redelivery",    "my_mqtt_root/energy/mains/power/L3_redelivery",      "L3 redelivery power" },
+  { "1-0:72.7.0",   METRIC_TYPE_FLOAT,  "V_L3",               "my_mqtt_root/energy/mains/voltage/L3",               "L3 voltage" },
+  { "1-0:71.7.0",   METRIC_TYPE_FLOAT,  "I_L3",               "my_mqtt_root/energy/mains/current/L3",               "L3 current" },
+  #endif
+
+  { "0-1:24.1.0",   METRIC_TYPE_BARE,       "",               "",   "gas meter device type" },
+  { "0-1:96.1.0",   METRIC_TYPE_META_TEXT,  "gas_meter_sn",   "",   "gas meter serial number" },
+
+  { "0-1:24.2.1",   METRIC_TYPE_GAS,    "gas_reading",    "my_mqtt_root/energy/gas/reading",  "gas meter last reading" },
+
+  { "0-0:96.7.21",  METRIC_TYPE_BARE,   "failures",       "my_mqtt_root/energy/mains/report/powerfailures",         "power failures" },
+  { "0-0:96.7.9",   METRIC_TYPE_BARE,   "long_failures",  "my_mqtt_root/energy/mains/report/powerfailures_long",    "long power failures" },
+  { "1-0:99.97.0",  METRIC_TYPE_OTHER,  "failure_log",    "my_mqtt_root/energy/mains/report/powerfailure_details",  "power failure event log" },
+  { "1-0:32.32.0",  METRIC_TYPE_BARE,   "L1_sags",        "my_mqtt_root/energy/mains/report/voltage_sags/L1",       "L1 voltage sags" },
+  { "1-0:32.36.0",  METRIC_TYPE_BARE,   "L1_swells",      "my_mqtt_root/energy/mains/report/voltage_swells/L1",     "L1 voltage swells" },
+  #ifdef THREE_PHASE
+  { "1-0:52.32.0",  METRIC_TYPE_BARE,   "L2_sags",        "my_mqtt_root/energy/mains/report/voltage_sags/L2",       "L2 voltage sags" },
+  { "1-0:52.36.0",  METRIC_TYPE_BARE,   "L2_swells",      "my_mqtt_root/energy/mains/report/voltage_swells/L2",     "L2 voltage swells" },
+  { "1-0:72.32.0",  METRIC_TYPE_BARE,   "L3_sags",        "my_mqtt_root/energy/mains/report/voltage_sags/L3",       "L3 voltage sags" },
+  { "1-0:72.36.0",  METRIC_TYPE_BARE,   "L3_swells",      "my_mqtt_root/energy/mains/report/voltage_swells/L3",     "L3 voltage swells" },
+  #endif
+
+  { "0-0:96.13.0",  METRIC_TYPE_TEXT,   "message",   "my_mqtt_root/energy/mains/message",   "text message" }  // text message
+};
